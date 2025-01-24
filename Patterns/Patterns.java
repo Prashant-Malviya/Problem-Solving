4f2ed7bdaa883b27e02a8fdd21858c6b7ca97029
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-01-24 10:55:32 +0530

    code updated

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -12,46 +12,45 @@ import java.util.*;
 
 public class Patterns {
 
+    static void pattern1(int rows, int cols) {
 
-    static void pattern1(int rows,int cols){
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-    static void pattern2(int rows){
+    static void pattern2(int rows) {
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<=i;j++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-    static void pattern3(int rows){
+    static void pattern3(int rows) {
         char c = 'A';
-        for(int i=1;i<=rows;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(c+" ");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c + " ");
             }
             c++;
             System.out.println();
         }
     }
 
-    static void pattern4(int rows){
+    static void pattern4(int rows) {
 
-        for(int i=0;i<rows;i++){
-            
+        for (int i = 0; i < rows; i++) {
+
             char startChar = (char) ('E' - i);
 
-            for(int j=0;j<=i;j++){
-                
+            for (int j = 0; j <= i; j++) {
+
                 System.out.print(startChar + " ");
                 startChar++;
             }
@@ -59,22 +58,100 @@ public class Patterns {
         }
     }
 
-    static void pattern5(int rows){
-
-        for(int i=1;i<=rows;i++){
-
-            for(int j=1;j<=rows-i-1;j++){
+    static void pattern5(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) { 
                 System.out.print(" ");
             }
-
-            for(int k=1;k<=i;k++){
+            for (int k = 1; k <= i; k++) { 
                 System.out.print(k);
+            }
+            System.out.println(); 
+        }
+    }
+
+    static void pattern6(int rows){
+        for(int i=rows;i >= 1;i--){
+            
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+    // mysirg trick-----------------
+
+    /*
+     *  *
+     *  **
+     *  ***
+     *  ****
+     *  *****
+     */
+
+    static void pattern7(int rows){
+        for(int i=1;i<=rows;i++){
+
+            for(int j=1;j<=rows;j++){
+
+                if(j<=i){
+                    System.out.print("$");
+                }else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
 
-    public static void main(String[] args){
+    /* Pattern 8
+
+    *
+   **
+  ***
+ ****
+*****
+     */
+
+    static void pattern8(int rows){
+        int newCols = rows+1;
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows;j++){
+                if(j>=newCols-i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /* Pattern 9
+     *  *****
+     *   ****
+     *    ***
+     *     **
+     *      *
+     */
+
+    static void pattern9(int rows){
+        
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows;j++){
+                if(j>=i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -90,12 +167,9 @@ public class Patterns {
         // pattern2(rows);
         // pattern3(rows);
         // pattern4(rows);
-        pattern5(rows);
+        // pattern5(rows);
+        // pattern7(rows);
+        pattern8(rows);
+        // pattern9(rows);
     }
 }
-
-
-
-
-
-
